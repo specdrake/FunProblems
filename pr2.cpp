1fcf757f958ae1bf7e86be233356c96917681700
Prashubh Atri <prashubh27@gmail.com>
2020-03-26 11:07:21 +0530

    redo pr2

--- pr2.cpp
+++ pr2.cpp
@@ -2,17 +2,12 @@
 using namespace std;
 int main(){
 	int a=0,b=1,c=0;
-	int n,ans=0,i=1;
+	int n,ans=0;
 	cin>>n;
-	if(n==1){
-		cout<<0<<endl;
-		return 0;
-	}
 	while(c<n){
-		if(i%2==0){
+		if(c%2==0){
 			ans+=c;
-// 			cout<<c<<endl;
-		}i++;
+		}
 		c=a+b;
 		a=b;b=c;
 	}

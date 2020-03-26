2d9ce0f81a008791cf286a917ebb25de61523e74
RIYUZAKI09 <62685992+RIYUZAKI09@users.noreply.github.com>
2020-03-26 10:51:21 +0530

    Update pr3.py

--- pr3.py
+++ pr3.py
@@ -1,12 +1,50 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number n entered by user?
-def sys
-calc(n):
-    #TODO
 
-if __name__ == '__main__':
-    #Accept user input and call calc()
-    #TODO
-    sys.exit(0)
 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool IfPrime(int n) 
+{ 
+    if (n <= 1) 
+        return false; 
+    if (n <= 3) 
+        return true; 
+
+    if (n % 2 == 0 || n % 3 == 0) 
+        return false; 
+  
+    for (int i = 5; i * i <= n; i = i + 6) 
+        if (n % i == 0 || n % (i + 2) == 0) 
+            return false; 
+  
+    return true; 
+}
+
+int LargestPrimeFactor(int n)
+{
+	int Largest = INT_MIN;
+	for(int i=2 ; i<=n ; i++)
+	{
+		if(n%i == 0 && IfPrime(i) && i>Largest)
+		{
+			Largest = i; 
+		}
+	}
+
+	return Largest;
+}
+
+int main(int argc, char const *argv[])
+{
+	int n;
+
+	cin>>n;
+
+	cout<<LargestPrimeFactor(n)<<endl;
+	return 0;
+}
 
